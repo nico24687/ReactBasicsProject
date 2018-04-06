bbe9c50d99457228531c12a173ebf7a8598f86a1
nico24687 <theonlynico@hotmail.com>
2018-04-06 08:07:55 -0600

    looping though array of players to generate Player components

--- app.jsx
+++ app.jsx
@@ -1,3 +1,21 @@
+var PLAYERS = [
+  {
+    name: "Nico Lewis",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Dexter Lewis",
+    score: 21,
+    id: 2,
+  },
+  {
+    name: "Melody Lewis",
+    score: 31,
+    id: 3,
+  }
+]
+
 function Header(props){
   return(
     <div className="header">
@@ -10,18 +28,28 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 }
 
+function Counter(props){
+  return(
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score">{props.score}</div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+}
+
 function Player(props){
   return(
     <div className="player">
       <div className="player-name">
         {props.name}
-          </div>
+      </div>
       <div className="player-score">
-        <div className="counter">
-          <button className="counter-action decrement"> - </button>
-          <div className="counter-score">{props.score}</div>
-          <button className="counter-action increment"> + </button>
-        </div>
+        <Counter score={props.score}/>
       </div>
     </div>
   )
@@ -39,8 +67,9 @@ function Application(props){
       <Header title={props.title} />
 
       <div className="players">
-        <Player  name="Nico Lewis" score={31}/>
-        <Player name="Dexter Lewis" score={21} />
+        {props.players.map(player => {
+          return <Player name={player.name} score={player.score} key={player.id}/>
+        })}
       </div>
 
     </div>
@@ -49,6 +78,11 @@ function Application(props){
 
 Application.propTypes = {
   title: React.PropTypes.string,
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired,
+  })).isRequired,
 }
 
 Application.defaultProps = {
@@ -56,7 +90,7 @@ Application.defaultProps = {
 }
 
 
-ReactDOM.render(<Application  />, document.getElementById('container'))
+ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'))
 
 
 
